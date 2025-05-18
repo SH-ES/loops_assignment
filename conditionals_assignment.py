01c0cd6e5bd03d50b9b0423c75842c70f8ab1ebb
SH-ES <shahina.hussains@gmail.com>
2025-05-18 09:24:27 +0500

    Add files via upload

---
+++ conditionals_assignment.py
@@ -0,0 +1,52 @@
+#TASK 1
+typing_speed=25
+if typing_speed>=30:
+    print("You are a good typist.")
+elif typing_speed<20:
+    print("You are advised to practise typing.")
+else:
+    print("Practise typing daily.")
+
+#TASK 3 VARIABLE MARKS
+marks=78
+if marks>=90:
+    print("Grade: A*")
+elif marks>=80:
+    print("Grade: A")
+elif marks>=70:
+    print("Grade B")
+else:
+    print("Try again")
+
+#TASK 4 WEATHER ADVICE
+Weather= "rainy"
+if Weather=="rainy":
+    print("Take an umbrella")
+if Weather=="sunny":
+    print("Wear sunglasses")
+if Weather=="cloudy":
+    print("It might rain later")
+
+#TASK 5:QUIZ-FEELING HUMGRY?
+hungry="False"
+if hungry=="True":
+    print("Go eat something!")
+if hungry=="False":
+    print("Maybe have a snack later.")
+
+BONUS FUN TASK
+
+mood =(input("How are you feeling today?"))
+if mood=="happy":
+    print("That's great to hear!")
+if mood=="sad":
+    print("I hope your day gets better.")
+if mood=="bored":
+    print("Try reading a book or going for a walk.")
+else:
+    print("Thanks for sharing your mood!")
+
+How are you feeling today?: happy
+
+    
+#I TRIED MULTIPLE TIMES. I WILL TRY AGAIN.
