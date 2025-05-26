8ecfd5b45a3a03239c2fc43fc896b7d103d6f28b
SH-ES <shahina.hussains@gmail.com>
2025-05-26 15:10:25 +0500

    Add files via upload

---
+++ Repractice10.py
@@ -0,0 +1,46 @@
+#TASK 1
+# for i in range(1,11):
+#     print(i)
+
+#TASK 2
+# i=1
+# while i<=5:
+#     print(i)
+#     i += 1
+
+#TASK 3
+# i=5
+# while i>=1:
+#     print(i)
+#     i -= 1
+
+#TASK 4
+# for i in range(1,5):
+#     for j in range(1,10):
+#         # print(f"i={i}, j={j}")
+#         print(f"{i}x{j}={i*j}")
+
+#TASK 5
+# i=0
+# while i <=10:
+#     print(i)
+#     if i==(6):
+#         break
+#     print(i)
+#     i+=1
+
+# for i in range(0,10):
+#     if i==7:
+#         break
+#     print(i)
+
+# TASK 6
+# for i in range(0,6):
+#     if i==3:
+#         continue
+#     print(i)
+
+# BONUS TASK
+name = "Shahina"
+for letter in name:
+    print(letter)
